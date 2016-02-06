2623256582e02f53dde54dc92f15ecbd5bc9042e
Serviox19 <servio19@gmail.com>
2016-02-06 14:05:43 -0500

    added github user search through path

--- js/server.js
+++ js/server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var PORT = 8080;
 var bodyParser = require('body-parser');
-//var session = require('express-session');
+var session = require('express-session');
+var GitHubApi = require("node-github");
+//var gitUsers = ["Serviox19", "Cbmoate"];
+
 
 //Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,10 +18,6 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/home.html');
 });
 
-//app.get('/login', function(req, res) {
-  //res.sendFile(process.cwd() + '/login.html');
-//});
-
 app.get('/my-account', function(req, res) {
   res.sendFile(process.cwd() + '/views/account-page.html');
 });
@@ -26,6 +25,22 @@ app.get('/my-account', function(req, res) {
 app.get('/help', function(req, res) {
   res.sendFile(process.cwd() + '/views/help.html');
 });
+
+app.get('/github/:user', function(req, res) {
+  var github = new GitHubApi({
+  version: "3.0.0"
+  });
+  
+  var user = req.params.user;
+
+  github.user.getFrom({
+    user: user
+  }, function(err, gitResponse){
+    res.send(JSON.stringify(gitResponse))
+  });
+});
+
+
 
 app.post('/', function(req, res) {
   console.log(req.body);
